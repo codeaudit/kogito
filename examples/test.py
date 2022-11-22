06833d8ea483a114609ec1b97ff0682b4eed342e
Mete Ismayil <mismayilza@gmail.com>
2022-11-22 23:51:36 +0100

    add diverse test results

---
+++ examples/test.py
@@ -0,0 +1,27 @@
+import argparse
+import pandas as pd
+from tqdm import tqdm
+import pathlib
+
+from kogito.inference import CommonsenseInference
+from kogito.models.bart.comet import COMETBART
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--datapath", type=str)
+    parser.add_argument("--output-dir", type=str)
+
+    args = parser.parse_args()
+
+    with open(args.datapath) as f:
+        data = f.readlines()
+
+    csi = CommonsenseInference()
+    model = COMETBART.from_pretrained()
+
+    for i, line in tqdm(enumerate(data), total=len(data), desc="Generating", position=0, leave=True):
+        kgraph = csi.infer(line, model)
+        kgraph.to_jsonl(f"{args.output_dir}/{pathlib.Path(args.datapath).stem}_{i+1}.json")
+    
+if __name__ == "__main__":
+    main()
